ea1eac04f898a3001f9c1b7a8d7e2b00e05e940a
DanielIgnacioCarcamo <carcamodaniel.informatica@gmail.com>
2023-04-16 19:23:29 -0400

    se le da formato: prettier.. aun sin funcionar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,18 @@
-let alucardVal = document.querySelector('#alucard-imp');
-let andersonVal = document.querySelector('#anderson-imp');
-let serasVal = document.querySelector('#seras-imp');
+let alucardVal = document.querySelector("#alucard-imp");
+let andersonVal = document.querySelector("#anderson-imp");
+let serasVal = document.querySelector("#seras-imp");
 const btn = document.querySelector("#evr");
 
-const cStickers = document.querySelector('#cantidad-stickers');
+const cStickers = document.querySelector("#cantidad-stickers");
 
 let total = +alucardVal + +andersonVal + +serasVal;
 
-btn.addEventListener('click', () => {
-    alert(+alucardVal + +andersonVal + +serasVal);
-});
-
-if (total => 10) {
- return cStickers.innerHTML("Llevas muchos stickers");
-}
-else if (total < 10 && total > 0) {
- return cStickers.innerHTML("Llevas "+total+" stickers");
-}
-
-else {
+btn.addEventListener("click", () => {
+  if ((total) => 10) {
+    return cStickers.innerHTML("Llevas muchos stickers");
+  } else if (total < 10 && total > 0) {
+    return cStickers.innerHTML("Llevas " + total + " stickers");
+  } else {
     return cStickers.innerHTML("intentalo de nuevo");
-}
+  }
+});
