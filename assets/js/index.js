288b062ec9fa43a5b5a515290daaec2f69884b5c
DanielIgnacioCarcamo <carcamodaniel.informatica@gmail.com>
2023-04-17 07:23:48 -0400

    se agrega el valor de value a las variables y se deja dentro de la funcion la operatoria para total

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,12 @@
-let alucardVal = document.querySelector("#alucard-imp");
-let andersonVal = document.querySelector("#anderson-imp");
-let serasVal = document.querySelector("#seras-imp");
-const btn = document.querySelector("#evr");
+let alucardVal = document.querySelector("#alucard-imp").value;
+let andersonVal = document.querySelector("#anderson-imp").value;
+let serasVal = document.querySelector("#seras-imp").value;
+const btn = document.querySelector("#evr").value;
 
 const cStickers = document.querySelector("#cantidad-stickers");
 
-let total = +alucardVal + +andersonVal + +serasVal;
-
 btn.addEventListener("click", () => {
+  let total = +alucardVal + +andersonVal + +serasVal;
   if ((total) => 10) {
     return cStickers.innerHTML("Llevas muchos stickers");
   } else if (total < 10 && total > 0) {
