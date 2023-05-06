97830364c3b6a9637bd8d9c1162b8f05b897895c
DanielIgnacioCarcamo <carcamodaniel.informatica@gmail.com>
2023-05-06 10:33:10 -0400

    arreglos desafio passwd

--- assets/js/passwd.js
+++ assets/js/passwd.js
@@ -5,14 +5,14 @@ function calcular() {
     const sel = document.querySelectorAll("select");
 
     if ((sel[0].value == 9) && (sel[1].value == 1) && (sel[2].value == 1)) {
-        return spanHtml[0] = "<p>password 1 correcto</p>";
+        return spanHtml.innerHTML = "<p>password 1 correcto</p>";
     }
 
     else if ((sel[0].value == 7) && (sel[1].value == 1) && (sel[2].value == 4)) {
-        return spanHtml[0] = "<p>password 2 correcto</p>";
+        return spanHtml.innerHTML = "<p>password 2 correcto</p>";
     }
 
     else {
-        return  spanHtml[0] = "<p>assword incorrecto</p>";
+        return  spanHtml.innerHTML =  "<p>assword incorrecto</p>";
     }
 }
