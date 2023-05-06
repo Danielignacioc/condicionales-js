b575eb621af21f15d9863837bc2ca5d6d64b03f5
DanielIgnacioCarcamo <carcamodaniel.informatica@gmail.com>
2023-05-06 10:39:39 -0400

    arreglos

--- assets/js/passwd.js
+++ assets/js/passwd.js
@@ -1,13 +1,13 @@
-const spanHtml = document.querySelectorAll("span");
+const spanHtml = document.getElementsByTagName("span");
 
 function calcular() {
   const sel = document.querySelectorAll("select");
 
   if (sel[0].value == 9 && sel[1].value == 1 && sel[2].value == 1) {
-    return (spanHtml.innerHTML = "<p>password 1 correcto</p>");
+    return (spanHtml[0].innerHTML = "<p>password 1 correcto</p>");
   } else if (sel[0].value == 7 && sel[1].value == 1 && sel[2].value == 4) {
-    return (spanHtml.innerHTML = "<p>password 2 correcto</p>");
+    return (spanHtml[0].innerHTML = "<p>password 2 correcto</p>");
   } else {
-    return (spanHtml.innerHTML = "<p>assword incorrecto</p>");
+    return (spanHtml[0].innerHTML = "<p>Password incorrecto</p>");
   }
 }
